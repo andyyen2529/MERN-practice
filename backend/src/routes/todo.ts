9381d7c4d67da7439d6b088b638005eeb4ec953f
andyyen2529 <andyyen2529@gmail.com>
2021-07-09 07:14:06 +0000

    add confit of test

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -12,6 +12,42 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
 
     // TODO: Add CRUD endpoints, i.e. get, post, update, delete
     // NOTE: the url should be RESTful
+    server.get('/todos', opts, async (request, reply) => {
+        try {
+            const todos: Array<ITodo> = await todoRepo.getTodos()
+            return reply.status(200).send({ todos})
+        } catch (error) {
+            console.error('GET /todos Error: ${error}')
+        }
+    })
+
+    server.post('/todos', opts, async (request, reply) => {
+        try {
+            const todoBody: ITodo = request.body as ITodo
+            const todo: ITodo = await todoRepo.addTodo(todoBody)
+            return reply.status(200).send({ todo })
+        } catch (error) {
+            console.error('GET /todos Error: ${error}')
+        }
+    })
+
+    // server.update('/todos', opts, async (request, reply) => {
+    //     try {
+    //         const todos: Array<ITodo> = await todoRepo.getTodos()
+    //         return reply.status(200).send({ todos})
+    //     } catch (error) {
+    //         console.error('GET /todos Error: ${error}')
+    //     }
+    // })
+
+    // server.delete('/todos', opts, async (request, reply) => {
+    //     try {
+    //         const todos: Array<ITodo> = await todoRepo.getTodos()
+    //         return reply.status(200).send({ todos})
+    //     } catch (error) {
+    //         console.error('GET /todos Error: ${error}')
+    //     }
+    // })
 
     done()
 }
